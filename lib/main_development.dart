6ca36a577b490efb8587372837ead41d376629c9
srineesh <srineesh.salur@gmail.com>
2021-10-21 02:42:29 +0530

    fixed the app_config.json file

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,7 +5,6 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:stagingapp/app/app.dart';
 import 'package:stagingapp/envinorment.dart';
 import 'package:stagingapp/main_staging.dart';
 
